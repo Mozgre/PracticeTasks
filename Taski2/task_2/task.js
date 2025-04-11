165f1771846d547d32cde19069acd3ecf90640a6
Mozgre <gaz5328@gmail.com>
2025-04-11 03:07:44 +0300

    init1

--- Taski2/task_2/task.js
+++
@@ -1,8 +0,0 @@
-//Написать функцию, которая на вход принимает два массива и возвращает новый массив
-//с элементами найденными только в одном массиве, но не в обоих.
-
-export function diffArrays(arr1, arr2) {
-    return arr1
-      .filter(x => !arr2.includes(x))
-      .concat(arr2.filter(x => !arr1.includes(x)));
-  }
--- Taski2/task_5/task.js
+++
@@ -1,8 +0,0 @@
-//Написать функцию, которая конвертирует строку в spinal-case регистр.
-
-export function spinalCase(str) {
-    return str
-      .replace(/([a-z])([A-Z])/g, '$1 $2')
-      .replace(/\s+|_+/g, '-')
-      .toLowerCase();
-  }
